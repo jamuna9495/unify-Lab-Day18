bc81138c7280f2cf331e193a6a655193f56805ed
Jamuna. K <jamuna9495@gmail.com>
2026-02-15 10:38:00 +0530

    Update script.JS

--- script.js
+++ script.js
@@ -4,4 +4,4 @@ console.log('Lab Session 18 Started');
 const display = document.getElementById('display');
 display.innerText = 'Logic Engine Online';
 
-// Practice your JS code here...
+// Practice your JS code here..
